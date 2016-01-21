26fa7cea132574c2d7372f1f547ddeaf40c6de63
lfeng1420 <lfeng1420@hotmail.com>
2016-01-21 19:32:39 +0800

    Update FroggerGame

--- Classes/FroggerGame.cpp
+++ Classes/FroggerGame.cpp
@@ -14,7 +14,22 @@ CFroggerGame::~CFroggerGame()
 //初始化
 void CFroggerGame::Init()
 {
+	//获取在选择游戏界面设置的速度和等级
+	m_iSpeed = GET_INTVALUE("SPEED", 0);
+	m_iLevel = GET_INTVALUE("LEVEL", 0);
 
+	//默认生命数为4
+	m_iLife = GET_INTVALUE("LIFE", 4);
+
+	//初始化当前分数，经过的赛车数量
+	m_iScore = 0;
+
+	//更新界面，分数、等级和生命
+	m_pGameScene->UpdateScore(m_iScore);
+	m_pGameScene->UpdateLevel(m_iLevel);
+	m_pGameScene->UpdateSmallBricks();
+
+	InitData();
 }
 
 //更新
@@ -68,5 +83,81 @@ void CFroggerGame::OnDownPressed()
 //Fire按下
 void CFroggerGame::OnFireBtnPressed()
 {
+
+}
+
+
+//更新所有河道
+void CFroggerGame::UpdateRivers(float dt)
+{
+	for (int i = ROW_NUM - 2; i >= m_iEndRiverIdx; --i)
+	{
+		//偶数河道默认全部填充，无需更新
+		if (i % 2 == 0)
+		{
+			continue;
+		}
+
+		UpdateRiver(i, dt);
+	}
+}
+
+//更新指定行的河道
+void CFroggerGame::UpdateRiver(int iRowIndex, float dt)
+{
+	RIVER& stRiver = m_mapRiver[iRowIndex];
+	
+	//更新时间
+	stRiver.iCurTime += dt;
+
+	//判断是否到了更新时间
+	if (stRiver.iCurTime >= stRiver.arrRefreshTime[stRiver.iCurState])
+	{
+		//重置时间
+		stRiver.iCurTime = 0;
+
+		//检查方向
+		if (stRiver.bLeft)
+		{
+			//向左移动，往最右边插入新Brick
+			for (int i = 0; i < COLUMN_NUM - 1; ++i)
+			{
+				m_arrBrickState[iRowIndex][i] = m_arrBrickState[iRowIndex][i + 1];
+			}
+			m_arrBrickState[iRowIndex][COLUMN_NUM - 1] = stRiver.iCurState;
+		}
+		else
+		{
+			//向右移动，往最左边插入新Brick
+			for (int i = COLUMN_NUM - 1; i > 0; --i)
+			{
+				m_arrBrickState[iRowIndex][i] = m_arrBrickState[iRowIndex][i - 1];
+			}
+			m_arrBrickState[iRowIndex][0] = stRiver.iCurState;
+		}
+
+		//计数增加，如果达到上限，则设置NextState
+		++stRiver.iCount;
+		if (stRiver.iCount == stRiver.arrMaxCount[stRiver.iCurState])
+		{
+			stRiver.iCurState = !stRiver.iCurState;
+			//重置计数
+			stRiver.iCount = 0;
+		}
+	}
+}
+
+
+//初始化数据、变量等
+void CFroggerGame::InitData()
+{
+	//设置初始所在位置
+	m_iSelfRowIdx = ROW_NUM - 1;
+	m_iSelfColIdx = COLUMN_NUM / 2 - 1;
+
+	//初始河道行上限
+	m_iEndRiverIdx = ROW_NUM / 2;
+
+	//初始化各个河道状态数据
 
 }
--- Classes/FroggerGame.h
+++ Classes/FroggerGame.h
@@ -40,6 +40,50 @@ public:
 	//---------------------    CSceneBase    ----------------------
 
 private:
-	
+	//更新所有河道
+	void UpdateRivers(float dt);
+
+	//更新指定行的河道
+	void UpdateRiver(int iRowIndex, float dt);
+
+	//初始化数据、变量等
+	void InitData();
+
+private:
+	struct RIVER
+	{
+		bool bLeft;				//是否向左边移动
+
+		int iCurState;			//下一个产生的Brick状态，0表示空
+
+		int iCount;				//当前产生Brick的个数
+
+		int arrMaxCount[2];		//对应状态产生的Brick最大数量
+
+		int arrRefreshTime[2];	//刷新时间
+
+		int iCurTime;			//当前时间
+	};
+
+	typedef map<int, RIVER> TMAP_RIVER;
+
+private:
+	TMAP_RIVER m_mapRiver;						//河道数据
+
+	bool m_arrBrickState[ROW_NUM][COLUMN_NUM];	//Brick状态
+
+	int m_iLife;								//生命数
+
+	int m_iSpeed;								//当前速度
+
+	int m_iLevel;								//当前等级
+
+	int m_iScore;								//当前分数
+
+	int m_iEndRiverIdx;							//河道最上层所在行数，每一个等级可能增加
+
+	int m_iSelfRowIdx;							//自己所在的行索引
+
+	int m_iSelfColIdx;							//自己所在的列索引
 };
 
